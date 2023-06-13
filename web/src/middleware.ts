a57ac6c5d353cd2e8c487bbe783ef0859a94b021
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-06-13 11:17:17 -0300

    fix: correcting middleware targeting after logging into production

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,20 +1,22 @@
-import { NextRequest, NextResponse } from 'next/server'
+// temporary fix for deployment
 
-const signInURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
+// import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value
+// const signInURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
 
-  if (!token) {
-    return NextResponse.redirect(signInURL, {
-      headers: {
-        'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20;`,
-      },
-    })
-  }
+// export function middleware(request: NextRequest) {
+//   const token = request.cookies.get('token')?.value
 
-  return NextResponse.redirect(new URL('/'))
-}
+//   if (!token) {
+//     return NextResponse.redirect(signInURL, {
+//       headers: {
+//         'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20;`,
+//       },
+//     })
+//   }
+
+//   return NextResponse.next()
+// }
 
 // export const config = {
 //   matcher: '/memories/:path*',
