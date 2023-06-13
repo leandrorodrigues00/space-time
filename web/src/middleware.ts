9de11e20f18c116e6d5899f93cb567ceda381515
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-06-13 11:24:09 -0300

    fix: correcting middleware targeting after logging into production

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,22 +1,22 @@
-// temporary fix for deployment
+/* eslint-disable no-unused-vars */
+import { NextRequest, NextResponse } from 'next/server'
 
-// import { NextRequest, NextResponse } from 'next/server'
+const signInURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
 
-// const signInURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value
 
-// export function middleware(request: NextRequest) {
-//   const token = request.cookies.get('token')?.value
+  // temporary fix for deployment
+  // if (!token) {
+  //   return NextResponse.redirect(signInURL, {
+  //     headers: {
+  //       'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20;`,
+  //     },
+  //   })
+  // }
 
-//   if (!token) {
-//     return NextResponse.redirect(signInURL, {
-//       headers: {
-//         'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20;`,
-//       },
-//     })
-//   }
-
-//   return NextResponse.next()
-// }
+  // return NextResponse.next()
+}
 
 // export const config = {
 //   matcher: '/memories/:path*',
