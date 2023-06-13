7403ae998f7a49ddc007795370466a60216d00cb
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-06-13 11:58:44 -0300

    fix: middleware in production

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -1,22 +1,17 @@
 import { api } from '@/lib/api'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
-  const code = searchParams.get('code')
-
-  const redirectTo = request.cookies.get('redirectTo')?.value
-
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url)
+  const code = searchParams.get('code') // get github code
+  const redirectTo = req.cookies.get('redirectTo')?.value
   const registerResponse = await api.post('/register', {
     code,
   })
 
   const { token } = registerResponse.data
-
-  const redirectURL =
-    redirectTo ?? new URL(process.env.RAILWAY_STATIC_URL || '/', request.url)
-
-  const cookieExpiresInSeconds = 60 * 60 * 24 * 30
+  const redirectURL = redirectTo ?? new URL('/', req.url) // if exists redirect, user goes to redirect else root
+  const cookieExpiresInSeconds = 60 * 60 * 24 * 30 // to expires in 30 days
 
   return NextResponse.redirect(redirectURL, {
     headers: {
